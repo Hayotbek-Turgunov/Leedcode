8492e725396e796b98229854e4e8527291475a0d
Hayotbek-Turgunov <hayotbekturgunov01gmail.com>
2024-03-03 19:57:11 +0500

    Sqrt(x)  // Runtime 71ms / 63.87% // Memory 52.66 MB / 37.12%

---
+++ Easy/Binary Search/#69 Sqrt(x).js
@@ -0,0 +1,12 @@
+// Solution - 1
+// Runtime 71ms / 63.87%
+// Memory 52.66 MB / 37.12%
+
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+  return Math.floor(Math.sqrt(x))
+};
