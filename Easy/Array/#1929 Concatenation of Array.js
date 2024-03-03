edd64b18514f647ec6fd1d5eef58005feafdaf36
Hayotbek-Turgunov <hayotbekturgunov01gmail.com>
2024-03-03 19:52:41 +0500

    Concatenation of Array

---
+++ Easy/Array/#1929 Concatenation of Array.js
@@ -0,0 +1,13 @@
+// Solution - 1
+// Runtime 62ms / 78.87%
+// Memory 51.41 MB / 56.42%
+
+
+// /**
+//  * @param {number[]} nums
+//  * @return {number[]}
+//  */
+
+var getConcatenation = function (nums) {
+  return nums.concat(nums)
+};
